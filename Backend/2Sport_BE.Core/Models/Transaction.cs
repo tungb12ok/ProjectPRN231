4ed15a4b6edc55de4249e27202191634eec7326a
anhdd17 <anhdd16.cdc@gmail.com>
2024-07-18 22:57:01 +0700

    upload api for update status checkout

---
+++ Backend/2Sport_BE.Core/Models/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2Sport_BE.Repository.Models
+{
+    public class Transaction
+    {
+        public string accountNo { get; set; }
+        public string deviceIdCommon { get; set; }
+        public string fromDate { get; set; }
+        public string historyNumber { get; set; }
+        public string historyType { get; set; }
+        public string refNo { get; set; }
+        public string sessionId { get; set; }
+        public string toDate { get; set; }
+        public string type { get; set; }
+    }
+}
